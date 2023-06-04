2c38fdf20b81403f56e95d974e20287a3a267216
skotpionx <skotpion15@gmail.com>
2023-06-04 18:19:32 +0200

    Importacion arreglada

--- pages/estudio.js
+++ pages/estudio.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import '../app/styles/estudio.css'
 import '../app/styles/font.css'
-import NavBar from '@/app/components/navBar';
-import Footer from '@/app/components/footer';
-import Recepcion from '@/app/components/Recepcion';
+import NavBar from '../app/components/navBar';
+import Footer from '../app/components/footer';
+import Recepcion from '../app/components/Recepcion';
 
 
 const Estudio = () => {
